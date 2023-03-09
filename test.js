f19c72ddac681bf542fa8ab410420556894bdb84
PhucMangue <126844674+PhucMangue@users.noreply.github.com>
2023-03-09 11:48:30 +0700

    upload

---
+++ test.js
@@ -0,0 +1,7 @@
+// const Button = document.querySelector('.js-translate');
+
+// function showEn(){
+//     document.getElementById('cmt').innerHTML="abc";
+// }
+ 
+// Button.addEventListener('click',showEn)
